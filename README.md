45d6c40b2db218adb6cacb20e7f018667dd7b286
gtarim <tarim.gokhan@gmail.com>
2022-04-01 23:52:23 +0300

    [refactor] project refactor by base_project repo

---
+++ README.md
@@ -0,0 +1,15 @@
+# C++ Cache Types Implementation
+
+The repo consist of some cache types implementation on c++
+mostly used on C++17 and plus
+
+## Idioms
+Here is the list of implemented cache types.
+- LRU (last recently usage)
+
+## Resources
+While developement mostly used below resources.
+
+- https://en.wikibooks.org/wiki/More_C%2B%2B_Idioms
+- StackOverFlow
+- https://codereview.stackexchange.com
--- main.cpp
+++ main.cpp
@@ -3,29 +3,5 @@
 
 int main()
 {
-    try
-    {
-        cache_lru<std::string, int> cache(4);
-
-        cache.add("gokhan", 30.1);
-        cache.add("haktan", 25);
-        cache.add("didem", 26);
-        cache.add("lale", 60);
-
-        cache.print();
-        cache.add("ersin", 65);
-        cache.print();
-        cache.add("yakup", 195);
-        cache.print();
-        cache.add("osman", 12195);
-        cache.print();
-
-        auto gokhan = cache.get("osman");
-        std::cout << *gokhan << "\n";
-    }
-    catch(...)
-    {
-        return 1;
-    }
-
+    std::cout << "hello world\n";
 }
